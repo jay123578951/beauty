dbce72724a42b70b1f84313202c9388ffb68dc64
jay <jay@ocard.co>
2024-10-18 18:41:02 +0800

    add: eslint tailwimdcss index page

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default {
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+}
